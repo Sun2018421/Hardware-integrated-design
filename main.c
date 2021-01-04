f54d2e894dacd030cef158a8a3c89bd3e080f43c
Sun2018421 <2475252204@qq.com>
2021-01-04 22:37:00 +0800

    add '(',')' and some bugs

--- main.c
+++ main.c
@@ -6,13 +6,20 @@ typedef unsigned int u16;	  //对数据类型进行声明定义
 typedef unsigned char u8;
 
 
-char Outputchar[18]={48,49,50,51,52,53,54,55,56,57,43,45,42,47,40,41,46,61};
-
-float a = 0.0 , b = 0.0, c =0.0 ,ans = 0.0;
+const char Outputchar[18]={48,49,50,51,52,53,54,55,56,57,43,45,42,47,40,41,46,61};
+#define MAX_LEN 4
+float a = 0.0 , b = 0.0, c =0.0 ;
+float pastdata[MAX_LEN];
+u8 Statestack[MAX_LEN];
+u8 stackpoint = -1, pastpoint = -1;
 u8 flag_a = 0 , flag_b = 0 , flag_c =0 ;
 u8 State = 0; //最开始在初始状态
 u8 add_sub_flag = 0 ; //1->'+' , 0->'-'
 u8 multi_div_flag = 0 ; //1->'*' , 0->'/'
+u8 count = 0 ; //对应括号的匹配
+
+// State : 0 0 0 0 0 0 0 0 -> 128 64 32 16 8 4 2 1
+void printans(float a);
 void initA(){
 	a = 0;
 	flag_a = 0;
@@ -25,6 +32,85 @@ void initC(){
 	c = 0 ;
 	flag_c = 0;
 }
+void pushState(){
+	switch(State){
+		case 0:   // (
+			stackpoint ++;
+			Statestack[stackpoint] = State;
+			break;
+		case 1:  // a +/- (
+			if(add_sub_flag == 1){
+				State += 128;
+			}
+			Statestack[++stackpoint]=State;
+			pastdata[++pastpoint] = a;
+			break;
+		case 3: // a +/- b */ (
+			if(add_sub_flag == 1)
+				State+=128;
+			if(multi_div_flag == 1)
+				State+=64;
+			Statestack[++stackpoint]=State;
+			pastdata[++pastpoint] = a;
+			pastdata[++pastpoint] = b;
+			break;
+		case 5: // a */ (
+			if(multi_div_flag == 1){
+				State += 64;
+			}
+			Statestack[++stackpoint]=State;
+			pastdata[++pastpoint] = a;
+			break;
+	}
+	initA();
+}
+
+void PopStack(){
+	u8 past = Statestack[stackpoint--];
+	switch(past&0x3F){ // 0011 1111
+		case 0:
+			State	= 0;
+			a = a;
+			break;
+		case 1:
+			State = 2;
+			if(past & 128 ==128){  // +
+				add_sub_flag = 1;
+			}
+			else 
+				add_sub_flag = 0;
+			b = a;
+			a = pastdata[pastpoint--];
+			break;
+		case 3:
+			State = 4;
+			if(past & 128 == 128){
+				add_sub_flag= 1;
+			}
+			else 
+				add_sub_flag = 0;
+			if(past & 64 == 64){
+				multi_div_flag = 1;
+			}
+			else
+				multi_div_flag = 0;
+			c = a;
+			b = pastdata[pastpoint--];
+			a = pastdata[pastpoint--];
+			break;
+		case 5:
+			State = 6;
+			if(past & 64 ==64){  // +
+				multi_div_flag = 1;
+			}
+			else 
+				multi_div_flag = 0;
+			c = a;
+			a = pastdata[pastpoint--];			
+			break;
+	}
+}
+
 void printans(float a){
 	u8 i ;
 	char str[8];
@@ -184,12 +270,6 @@ void function_S0(){
 		LcdWriteData(Outputchar[num]);
 		printans(a);
 	}
-	else if(num == 16){
-		if(flag_a == 0){
-			flag_a = 1;
-			LcdWriteData(Outputchar[num]);
-		}
-	}	
 	else if(num == 18){
 	clear();
 	}
@@ -203,23 +283,31 @@ void function_S0(){
 					flag_a ++;
 			}
 		}
+		else if(num == 16){
+			if(flag_a == 0){
+				flag_a = 1;
+			}
+			}
+		else if(num == 14){
+			pushState();
+			State = 0;
+		}
+		else if(num == 15){
+			PopStack();
+		}
 		else if(num == 10){  // + -
-	//		initB();
 			add_sub_flag = 1;
 			State = 1;
 		}
 		else if(num == 11){
-	//		initB();
 			add_sub_flag = 0 ;
 			State = 1;
 		}
 		else if(num ==12 ){
-//			initC();
 			State = 5;
 			multi_div_flag = 1;
 		}// * /
 		else if(num == 13){
-	//		initC();
 			State =5 ;
 			multi_div_flag = 0;
 		}
@@ -236,6 +324,11 @@ void function_S1(){
 	if(isNum(num)==1){
 		b = num;
 		State = 2;
+		LcdWriteData(Outputchar[num]);
+	}
+	else if(num == 14){
+		pushState();
+		State = 0;
 		LcdWriteData(Outputchar[num]);
 	}
 	else if(num == 18){
@@ -258,12 +351,6 @@ void function_S2(){
 	else if(num == 18){
 		clear();
 	}
-	else if(num == 16){
-		if(flag_b == 0){
-			flag_b = 1;
-			LcdWriteData(Outputchar[num]);
-		}
-	}	
 	else{
 		if(isNum(num) == 1){
 			if(flag_b == 0){
@@ -275,12 +362,22 @@ void function_S2(){
 			}
 		}
 		else if (num== 12){  //*
-		//	initC();
 			State = 3;
 			multi_div_flag = 1;
 		}
+		else if(num == 15){  // )
+		if(add_sub_flag == 1)
+			a = a+b ;
+		else a = a-b;
+			
+		PopStack();
+		}
+		else if(num == 16){
+		if(flag_b == 0){
+			flag_b = 1;
+			}
+		}	
 		else if (num == 13){ // /
-			//initC();
 			State = 3; 
 			multi_div_flag = 0;
 		}
@@ -312,6 +409,11 @@ void function_S3(){
 	if(isNum(num)==1){
 		c = num;
 		State = 4;
+		LcdWriteData(Outputchar[num]);
+	}
+	else if(num == 14){
+		pushState();
+		State = 0;
 		LcdWriteData(Outputchar[num]);
 	}
 	else if(num == 18){
@@ -348,8 +450,6 @@ void function_S4(){
 			else
 				a= a-b/c;
 		}
-//		initB();
-//		initC();
 		add_sub_flag = 1;
 		State = 1;
 	}
@@ -366,8 +466,6 @@ void function_S4(){
 			else
 				a= a-b/c;
 		}
-//		initB();
-//		initC();
 		add_sub_flag = 0;
 		State = 1;
 	}
@@ -377,7 +475,6 @@ void function_S4(){
 		}
 		else 
 			b = b/c;
-//		initC();
 		State =3;
 		multi_div_flag = 1;
 	}
@@ -387,7 +484,6 @@ void function_S4(){
 		}
 		else 
 			b = b/c;
-//		initC();
 		State =3;
 		multi_div_flag = 0;
 	}
@@ -404,12 +500,24 @@ void function_S4(){
 			else
 				a= a-b/c;
 		}
-//		initB();
-//		initC();
 		add_sub_flag = 0;
 		State = 0;
 	}
-	
+	else if(num == 15){
+		if(add_sub_flag == 1){
+			if(multi_div_flag ==1)
+				a= a+b*c;
+			else
+				a= a+b/c;
+		}
+		else{ 
+			if(multi_div_flag ==1)
+				a= a-b*c;
+			else
+				a= a-b/c;
+		}
+		PopStack();
+	}
 	if(num!=17){
 		LcdWriteData(Outputchar[num]);
 	}
@@ -428,6 +536,11 @@ void function_S5(){
 	if(isNum(num)==1){
 		c = num;
 		State = 6;
+		LcdWriteData(Outputchar[num]);
+	}
+	else if(num == 14){
+		pushState();
+		State = 0;
 		LcdWriteData(Outputchar[num]);
 	}
 	else if(num==18){
@@ -462,7 +575,6 @@ void function_S6(){
 			State = 1;
 			add_sub_flag = 1;	
 		}
-//		initC();
 	}
 	else if (num == 11){ // -
 		if(multi_div_flag==1){
@@ -475,7 +587,6 @@ void function_S6(){
 			State = 1;
 			add_sub_flag = 0;	
 		}
-//		initC();
 	}
 	else if(num ==12){ // *
 		if(multi_div_flag==1){
@@ -487,7 +598,6 @@ void function_S6(){
 			multi_div_flag = 1;	
 		}
 		State = 5;
-//		initC();
 	}
 	else if (num == 13){ // /
 		if(multi_div_flag==1){
@@ -498,7 +608,6 @@ void function_S6(){
 			a = a/c;
 			multi_div_flag = 0;	
 		}
-//		initC();
 		State = 5;
 	}
 	else if(num == 17){ // ==
@@ -509,8 +618,15 @@ void function_S6(){
 		else{
 			a = a/c;
 		}
-//		initB();
-//		initC();
+	}
+	else if(num == 15){
+		if(multi_div_flag==1){
+			a = a*c;
+		}
+		else{
+			a = a/c;
+		}
+		PopStack();
 	}
 	
 	if(num == 17){
@@ -523,9 +639,7 @@ void function_S6(){
 		LcdWriteData(Outputchar[num]);
 	}
 }
-void function_S7(){
-	u8 num = Getch();
-}
+
 //todo : a 的flag没有更新过
 void main(){
 	LcdInit();
@@ -552,9 +666,6 @@ void main(){
 			case 6:
 				function_S6();
 				break;
-			case 7:
-				function_S7(); //结果状态
-				break;	
 		}
 	}
 }
